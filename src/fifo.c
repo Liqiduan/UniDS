53d03c5602d46eb20dc4ea5699e5aca1f9c9f468
Liqiduan <lee.nju@live.com>
2016-04-01 21:54:49 +0800

    Need to finish uart and fifo!

---
+++ src/fifo.c
@@ -0,0 +1,125 @@
+/**
+ * @file fifo.c
+ * @brief Implementation of fifo.
+ * @author Liqiduan
+ * @version 1.0.0
+ * @date 2016-04-01
+ */
+
+#include "fifo.h"
+
+/**
+ * @brief Initial memory for fifo to use. 
+ *
+ * @param p 	 Point to the fifo will be initialled.
+ * @param len	 Indicate how many memory will be used.
+ * @param dataP  Point to the memory will be used.
+ *
+ * Initial memory from dataP to dataP + len*sizeof(fifo_data_t)
+ * 
+ * 
+ * @return 0 - Success 
+ * 	   -1 - failed
+ */
+
+int fifo_init(fifo *p, fifo_len_t len, fifo_data_t *dataP)
+{
+	if(len<=MAX_FIFO_SIZE)
+	{
+		p->readP=dataP;
+		p->writeP=dataP;
+		p->beginP=dataP;
+		p->endP=p->beginP + len;
+	}
+}
+
+
+/**
+ * @brief fifo_used Return the size has been used
+ *
+ * @param p Point to the fifo
+ *
+ * @return Size has been used 
+ */
+fifo_len_t fifo_used(fifo *p)
+{
+	if( p->readP >= p->writeP )
+	{
+		return (p->readP - p->writeP); 
+	}
+	else
+	{
+		return  (p->endP - p->beginP) - (p->writeP - p->readP);
+	}
+}
+
+
+/**
+ * @brief fifo_clear Clear a fifo
+ *
+ * @param p Point to the fifo
+ */
+void fifo_clear(fifo *p)
+{
+	p->readP=p->beginP;
+	p->writeP=p->endP;
+}
+
+
+/**
+ * @brief fifo_gets Get data from fifo
+ *
+ * @param p 	Point to the fifo which will be read
+ * @param len	Indicate how many data will be read
+ * @param bufP	The data read from fifo will store in the memory pointed by bufP
+ *
+ * @return 	How many data has been get
+ */
+int fifo_gets(fifo *p, fifo_len_t len, fifo_data_t *bufP)
+{
+	fifo_len_t count=0;
+	while(p->readP!=p->writeP)
+	{
+		if(p->readP > p->endP)
+		{
+			p->readP=p->beginP;
+		}
+		else
+		{
+			*bufP = *p->readP;
+			count++;
+			p->readP++;
+			bufP++;
+		}
+	}
+	return count;
+}
+
+/**
+ * @brief fifo_puts Put data into fifo
+ *
+ * @param p	Point to the fifo will be put
+ * @param len	Indicate how many data will be put
+ * @param bufP	Point to the data will be put
+ *
+ * @return How many data has put
+ */
+int fifo_puts(fifo *p, fifo_len_t len, fifo_data_t *bufP)
+{
+	fifo_len_t count=0;
+	while(p->readP!=(p->writeP+1))
+	{
+		if(p->writeP > p->endP)
+		{
+			p->writeP=p->beginP;
+		}
+		else
+		{
+			*p->writeP = *bufP;
+			count++;
+			p->writeP++;
+			bufP++;
+		}
+	}
+	return count;
+}
---
+++ src/fifo.h
@@ -0,0 +1,55 @@
+/**
+ * @file fifo.h
+ * @brief Header file of fifo part.
+ * @author Liqiduan
+ * @version 1.0.0
+ * @date 2016-04-01
+ */
+
+#ifndef _UNIDS_FIFO_H_
+#define _UNIDS_FIFO_H_
+
+#define MAX_FIFO_SIZE 32
+
+typedef unsigned int fifo_len_t;
+typedef unsigned int fifo_data_t;
+
+/**
+ * @brief Here is the struct definiton of fifo
+ *
+ */
+typedef struct
+{
+	fifo_data_t readP;	///Point to the memory will be read
+	fifo_data_t writeP;	///Point to the memory will be write
+	fifo_data_t beginP;	///Point to the begin of a memory used as fifo.
+	fifo_data_t endP;	///Point to the end of a memory used as fifo.
+}fifo;
+
+/**
+ * @brief Initial memory for fifo to use.
+ */
+int fifo_init(fifo *p, fifo_len_t len, fifo_data_t *dataP);
+
+/**
+ * @brief Return how many memory has been used!
+ */
+fifo_len_t fifo_used(fifo *p);
+
+/**
+ * @brief Clear a fifo.
+ */
+void fifo_clear(fifo *p);
+
+/**
+ * @brief Get data from fifo.
+ */
+int fifo_gets(fifo *p, fifo_len_t len, fifo_data_t *bufP);
+
+/**
+ * @brief Put data into a fifo.
+ */
+int fifo_puts(fifo *p, fifo_len_t len, fifo_data_t *bufP);
+
+#endif
+
